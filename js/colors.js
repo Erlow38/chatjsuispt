bcf4dde7066025adf71dcc35c4d9553c380d679e
Erlow <98266516+Erlow38@users.noreply.github.com>
2023-06-13 22:31:20 +0200

    Add files via upload

--- js/colors.js
+++ js/colors.js
@@ -3,4 +3,5 @@ let colors_input = document.querySelector('.colors-container input');
 colors_input.addEventListener('change', function() {
     document.documentElement.style.setProperty('--primary-color', this.value);
     document.documentElement.style.setProperty('--secondary-color', this.value + '80');
+    console.log('Color changed to ' + this.value + '80');
 });
