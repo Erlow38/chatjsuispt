a0da493c4755d1364e6245c665e563ec6081de5a
Mei <belgrandlaureen.tg2@gmail.com>
2023-06-17 11:59:04 +0200

    aaa

--- js/message.js
+++ js/message.js
@@ -25,10 +25,12 @@ function addMessage() {
 
         // Scroll to the bottom of the chat
         chat_container.scrollTop = chat_container.scrollHeight;
-        
+
+        // Generate the bot response
+        let response = 'Bienvenido a la página de contacto de la empresa. ¿En qué podemos ayudarte?';
+            
         setTimeout(function() {
             // Show the bot response in the chat
-            let response = 'Bienvenido a la página de contacto de la empresa. ¿En qué podemos ayudarte?';
             let new_bot_message = document.createElement('div');
             new_bot_message.classList.add('message', 'bot-message');
             new_bot_message.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="fill: rgba(242, 242, 242, 1);"><path d="M21.928 11.607c-.202-.488-.635-.605-.928-.633V8c0-1.103-.897-2-2-2h-6V4.61c.305-.274.5-.668.5-1.11a1.5 1.5 0 0 0-3 0c0 .442.195.836.5 1.11V6H5c-1.103 0-2 .897-2 2v2.997l-.082.006A1 1 0 0 0 1.99 12v2a1 1 0 0 0 1 1H3v5c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5a1 1 0 0 0 1-1v-1.938a1.006 1.006 0 0 0-.072-.455zM5 20V8h14l.001 3.996L19 12v2l.001.005.001 5.995H5z"></path><ellipse cx="8.5" cy="12" rx="1.5" ry="2"></ellipse><ellipse cx="15.5" cy="12" rx="1.5" ry="2"></ellipse><path d="M8 16h8v2H8z"></path></svg>';
@@ -77,6 +79,7 @@ for (let i = 0; i < suggestions.length; i++) {
     });
 }
 
+// Listen for the delete chat button click
 delete_chat_button.addEventListener('click', function() {
     //supprimer les elements possedant la classe message
     let messages = document.querySelectorAll('.message');
